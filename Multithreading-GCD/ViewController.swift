e234b6877cd4becb72ff1ee853b865f7e7154fb3
Rus <de.mr44@gmail.com>
2021-10-25 23:50:09 +0500

    Added an animated image to monitor the main thread

--- Multithreading-GCD/ViewController.swift
+++ Multithreading-GCD/ViewController.swift
@@ -7,17 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var gearImageView: UIImageView!
+    @IBOutlet weak var mainImageView: UIImageView!
     @IBOutlet weak var downloadButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         downloadButton.layer.cornerRadius = 15
+        animateGear()
     }
     
+    private func animateGear() {
+        guard let image = UIImage(systemName: "gearshape") else { return }
+        gearImageView.image = image
+        gearImageView.tintColor = .gray
+        UIView.animate(withDuration: 2.0, delay: 0.0, options: [.curveLinear, .repeat]) {
+            self.gearImageView.transform = CGAffineTransform(rotationAngle: -(3 * .pi) / 2)
+        }
+    }
+        
     @IBAction func downloadButtonAction(_ sender: UIButton) {
         
     }
