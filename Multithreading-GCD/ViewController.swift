ac5f7b6b93405f1f4253e8ffbf46a1e83a134bc4
Rus <de.mr44@gmail.com>
2021-10-27 23:44:35 +0500

    Added some different URLs with images

--- Multithreading-GCD/ViewController.swift
+++ Multithreading-GCD/ViewController.swift
@@ -15,7 +15,14 @@ final class ViewController: UIViewController {
     
     private let queue = DispatchQueue.global(qos: .utility)
     
-    private let imageURL = URL(string: "https://images.unsplash.com/photo-1635156219587-879ded59e273?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2742&q=80")!
+    private let imageURLArray: [URL] = [
+        URL(string: "https://images.unsplash.com/photo-1635156219587-879ded59e273?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2742&q=80")!,
+        URL(string: "https://images.unsplash.com/photo-1635355995448-77b02d33621d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2670&q=80")!,
+        URL(string: "https://images.unsplash.com/photo-1635333607240-1397692acdd2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=927&q=80")!,
+        URL(string: "https://images.unsplash.com/photo-1635353779138-0fddba218a30?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1365&q=80")!,
+        URL(string: "https://images.unsplash.com/photo-1635331987063-ed4700a9880d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1356&q=80")!,
+        URL(string: "https://images.unsplash.com/photo-1635282040419-b4948d5c064c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1323&q=80")!
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,7 +45,7 @@ final class ViewController: UIViewController {
         gearImageView.isHidden = false
         mainImageView.image = nil
         queue.async {
-            if let data = try? Data(contentsOf: self.imageURL) {
+            if let data = try? Data(contentsOf: self.imageURLArray.randomElement()!) {
                 DispatchQueue.main.async {
                     self.mainImageView.image = UIImage(data: data)
                     self.gearImageView.isHidden = true
