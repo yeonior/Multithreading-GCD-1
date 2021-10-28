8510b96ec63f27791701de02914e007cfe9155c8
Rus <de.mr44@gmail.com>
2021-10-28 18:46:39 +0500

    Some animation improvements

--- Multithreading-GCD/ViewController.swift
+++ Multithreading-GCD/ViewController.swift
@@ -14,6 +14,8 @@ final class ViewController: UIViewController {
     @IBOutlet weak var downloadButton: UIButton!
     
     private let queue = DispatchQueue.global(qos: .utility)
+    private var animator: UIViewPropertyAnimator!
+    private var firstLaunching = true
     
     private let imageURLArray: [URL] = [
         URL(string: "https://images.unsplash.com/photo-1635156219587-879ded59e273?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2742&q=80")!,
@@ -33,15 +35,32 @@ final class ViewController: UIViewController {
     }
     
     private func animateGear() {
-        guard let image = UIImage(systemName: "gearshape") else { return }
-        gearImageView.image = image
-        gearImageView.tintColor = .gray
-        UIView.animate(withDuration: 2.0, delay: 0.0, options: [.curveLinear, .repeat]) {
-            self.gearImageView.transform = CGAffineTransform(rotationAngle: -(3 * .pi) / 2)
+        if firstLaunching {
+            guard let image = UIImage(systemName: "gearshape") else { return }
+            gearImageView.image = image
+            gearImageView.tintColor = .gray
+//            animator = UIViewPropertyAnimator(duration: 1, curve: .linear, animations: {
+//                UIView.animate(withDuration: 1, delay: 0.0, options: [.curveLinear, .repeat]) {
+//                    self.gearImageView.transform = CGAffineTransform(rotationAngle: -(3 * .pi) / 2)
+//                }
+//            })
+            animator = UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 2.0,
+                                                                      delay: 0.0,
+                                                                      options: [.curveLinear,.repeat],
+                                                                      animations: {
+                                                                        self.gearImageView.transform = CGAffineTransform(rotationAngle: -(3 * .pi) / 2)
+                                                                      })
+            firstLaunching = false
+            print("start")
         }
+        animator.startAnimation()
     }
         
     @IBAction func downloadButtonAction(_ sender: UIButton) {
+        if !self.firstLaunching {
+            print("continue")
+            self.animator.startAnimation()
+        }
         gearImageView.isHidden = false
         mainImageView.image = nil
         queue.async {
@@ -49,6 +68,8 @@ final class ViewController: UIViewController {
                 DispatchQueue.main.async {
                     self.mainImageView.image = UIImage(data: data)
                     self.gearImageView.isHidden = true
+                    print("pause")
+                    self.animator.stopAnimation(true)
                 }
             }
         }
