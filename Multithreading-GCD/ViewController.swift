a3648936329ddd6a5a6a35ff6f474e5e3efac322
Rus <de.mr44@gmail.com>
2021-10-29 16:22:31 +0500

    Changed the imageView to the activityIndicatorView

--- Multithreading-GCD/ViewController.swift
+++ Multithreading-GCD/ViewController.swift
@@ -9,13 +9,12 @@ import UIKit
 
 final class ViewController: UIViewController {
 
-    @IBOutlet weak var gearImageView: UIImageView!
+    
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
     @IBOutlet weak var mainImageView: UIImageView!
     @IBOutlet weak var downloadButton: UIButton!
     
     private let queue = DispatchQueue.global(qos: .utility)
-    private var animator: UIViewPropertyAnimator!
-    private var firstLaunching = true
     
     private let imageURLArray: [URL] = [
         URL(string: "https://images.unsplash.com/photo-1635156219587-879ded59e273?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2742&q=80")!,
@@ -30,46 +29,21 @@ final class ViewController: UIViewController {
         super.viewDidLoad()
         
         downloadButton.layer.cornerRadius = 15
-        gearImageView.isHidden = true
-        animateGear()
-    }
-    
-    private func animateGear() {
-        if firstLaunching {
-            guard let image = UIImage(systemName: "gearshape") else { return }
-            gearImageView.image = image
-            gearImageView.tintColor = .gray
-//            animator = UIViewPropertyAnimator(duration: 1, curve: .linear, animations: {
-//                UIView.animate(withDuration: 1, delay: 0.0, options: [.curveLinear, .repeat]) {
-//                    self.gearImageView.transform = CGAffineTransform(rotationAngle: -(3 * .pi) / 2)
-//                }
-//            })
-            animator = UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 2.0,
-                                                                      delay: 0.0,
-                                                                      options: [.curveLinear,.repeat],
-                                                                      animations: {
-                                                                        self.gearImageView.transform = CGAffineTransform(rotationAngle: -(3 * .pi) / 2)
-                                                                      })
-            firstLaunching = false
-            print("start")
-        }
-        animator.startAnimation()
+        activityIndicatorView.isHidden = true
     }
         
     @IBAction func downloadButtonAction(_ sender: UIButton) {
-        if !self.firstLaunching {
-            print("continue")
-            self.animator.startAnimation()
-        }
-        gearImageView.isHidden = false
         mainImageView.image = nil
+        activityIndicatorView.isHidden = false
+        print("start")
+        activityIndicatorView.startAnimating()
         queue.async {
             if let data = try? Data(contentsOf: self.imageURLArray.randomElement()!) {
                 DispatchQueue.main.async {
                     self.mainImageView.image = UIImage(data: data)
-                    self.gearImageView.isHidden = true
-                    print("pause")
-                    self.animator.stopAnimation(true)
+                    self.activityIndicatorView.isHidden = true
+                    print("stop")
+                    self.activityIndicatorView.stopAnimating()
                 }
             }
         }
