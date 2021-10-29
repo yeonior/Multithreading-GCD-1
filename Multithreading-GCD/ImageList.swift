80d6b7ab2d65c9d075b9a15db94ce3665ede2486
Rus <de.mr44@gmail.com>
2021-10-29 16:30:22 +0500

    Some optimizations

---
+++ Multithreading-GCD/ImageList.swift
@@ -0,0 +1,20 @@
+//
+//  ImageList.swift
+//  Multithreading-GCD
+//
+//  Created by ruslan on 29.10.2021.
+//
+
+import Foundation
+
+final class ImageList {
+    
+    static let list: [URL] = [
+        URL(string: "https://images.unsplash.com/photo-1635156219587-879ded59e273?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2742&q=80")!,
+        URL(string: "https://images.unsplash.com/photo-1635355995448-77b02d33621d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2670&q=80")!,
+        URL(string: "https://images.unsplash.com/photo-1635333607240-1397692acdd2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=927&q=80")!,
+        URL(string: "https://images.unsplash.com/photo-1635353779138-0fddba218a30?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1365&q=80")!,
+        URL(string: "https://images.unsplash.com/photo-1635331987063-ed4700a9880d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1356&q=80")!,
+        URL(string: "https://images.unsplash.com/photo-1635282040419-b4948d5c064c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1323&q=80")!
+    ]
+}
--- Multithreading-GCD/ViewController.swift
+++ Multithreading-GCD/ViewController.swift
@@ -8,22 +8,12 @@
 import UIKit
 
 final class ViewController: UIViewController {
-
     
     @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
     @IBOutlet weak var mainImageView: UIImageView!
     @IBOutlet weak var downloadButton: UIButton!
     
     private let queue = DispatchQueue.global(qos: .utility)
-    
-    private let imageURLArray: [URL] = [
-        URL(string: "https://images.unsplash.com/photo-1635156219587-879ded59e273?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2742&q=80")!,
-        URL(string: "https://images.unsplash.com/photo-1635355995448-77b02d33621d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2670&q=80")!,
-        URL(string: "https://images.unsplash.com/photo-1635333607240-1397692acdd2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=927&q=80")!,
-        URL(string: "https://images.unsplash.com/photo-1635353779138-0fddba218a30?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1365&q=80")!,
-        URL(string: "https://images.unsplash.com/photo-1635331987063-ed4700a9880d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1356&q=80")!,
-        URL(string: "https://images.unsplash.com/photo-1635282040419-b4948d5c064c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1323&q=80")!
-    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,7 +28,7 @@ final class ViewController: UIViewController {
         print("start")
         activityIndicatorView.startAnimating()
         queue.async {
-            if let data = try? Data(contentsOf: self.imageURLArray.randomElement()!) {
+            if let data = try? Data(contentsOf: ImageList.list.randomElement()!) {
                 DispatchQueue.main.async {
                     self.mainImageView.image = UIImage(data: data)
                     self.activityIndicatorView.isHidden = true
